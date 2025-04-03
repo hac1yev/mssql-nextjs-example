31383cb4c8af4ab3b4151f76623bd2004c28f0e5
hac1yev <ilkinhaciyev955@gmail.com>
2025-04-03 14:35:24 +0400

    installed stripe packages created pages

---
+++ app/payment/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PaymentPage = () => {
+    const b = 10;
+
+    return (
+        <div className='d-flex align-items-center justify-content-center my-3 py-0'>
+            
+        </div>
+    );
+};
+
+export default PaymentPage;
